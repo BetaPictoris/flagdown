e6aef4c36421b2ba2be386b388b0706b4f7cfd4b
Daniel <beta@ozx.me>
2022-11-16 19:52:53 +0000

    Added basic API server

---
+++ api/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+
+  "github.com/gofiber/fiber/v2"
+)
+
+func main() {
+  log.Println("Initializing API...")
+  app := fiber.New(fiber.Config{
+    ServerHeader:  "Flagdown API Server",
+		CaseSensitive: true,
+		AppName: "Flagdown",
+	})
+
+  v1api := app.Group("/api").Group("/v1")
+
+  /*
+  GET: /api/v1/ping
+
+  Checks if the API Server is running
+  */
+  v1api.Get("/ping", func(c *fiber.Ctx) error {
+    c.SendStatus(200)
+    return c.SendString("Pong!")
+  })
+
+  log.Println("Starting API...")
+  app.Listen(":3000")
+}
