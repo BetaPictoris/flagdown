8600096745032d642fd65d66ed4653b7b06953c8
Daniel <beta@ozx.me>
2022-11-18 00:57:15 +0000

    Made `/api` ping sanity check

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,7 +1,24 @@
+import React from 'react';
+import axios from 'axios';
+
 export default function App() {
+  const [data, setData] = React.useState('Waiting...');
+
+  const handlePing = async () => {
+      try {
+          const response = await axios.get('/api/v1/ping');
+          setData(response.data);
+      } catch (e) {
+        setData('Failed to ping');
+      }
+
+      setTimeout(() => setData('Waiting...'), 2000);
+  }
+
   return (
     <div className="App">
-      Flagdown client
+      <div><pre>{data}</pre></div>
+      <button onClick={handlePing}>Ping</button>
     </div>
   );
 }
