34932e8d4054ec303e28b1d9ea319073103cf5c7
Beta Pictoris <beta@ozx.me>
2022-12-08 17:19:07 -0800

    Added, and fixed, details of Flag comp

--- app/components/Flag.js
+++ app/components/Flag.js
@@ -22,7 +22,14 @@ export default function Flag(props) {
         </AccordionButton>
       </h2>
       <AccordionPanel pb={4}>
-        Value: <Code>{props.flag.Value}</Code>
+        <div className="flag">
+          {props.flag.Description ? props.flag.Description : "This flag has no description."}
+        </div>
+        
+        <div className="flagValueBox">
+          Value: <Code>{props.flag.Value}</Code>
+        </div>
+        
         <DeleteFlagPrompt
           projectName={props.project.ProjectName}
           flagName={props.flag.flagName}
