0300a4029873a076cfaa9f2087c6854121d8032c
Daniel <beta@ozx.me>
2022-12-11 08:14:02 +0000

    Cleaned, and fixed, ProjectSettings.

--- app/components/ProjectSettings.js
+++ app/components/ProjectSettings.js
@@ -1,12 +1,18 @@
-import {Heading} from "@chakra-ui/react"
+import {Box, Heading} from "@chakra-ui/react"
 
 import DeleteProjectPrompt from "./DeleteProjectPrompt";
 
+import "./styles/ProjectSettings.scss"
 
 export default function ProjectSettings(props) {
-  <div className="ProjectSettings">
-    <Heading as="h2">{props.project.ProjectName} settings</Heading>
+  return (
+    <div className="ProjectSettings">
+      <Heading as="h2">{props.project.ProjectName} settings</Heading>
 
-    <DeleteProjectPrompt projectID={props.project.projectID} />
-  </div>
+      <Heading as="h3">Danager Zone</Heading>
+      <Box className="dangerZone" borderWidth='1px' borderRadius='lg' overflow='hidden' borderColor='tomato'>
+        <DeleteProjectPrompt projectID={props.project.projectID} />
+      </Box>
+    </div>
+  )
 }
