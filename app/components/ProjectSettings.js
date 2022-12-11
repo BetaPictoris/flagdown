9440535a9ca296a5882b7bad63b6975c5c929fdc
Beta Pictoris <beta@ozx.me>
2022-12-11 14:02:58 -0800

    Added rename project prompt

--- app/components/ProjectSettings.js
+++ app/components/ProjectSettings.js
@@ -1,5 +1,6 @@
 import { Box, Heading } from "@chakra-ui/react";
 
+import RenameProjectPrompt from "./RenameProjectPrompt";
 import DeleteProjectPrompt from "./DeleteProjectPrompt";
 
 import "./styles/ProjectSettings.scss";
@@ -7,6 +8,15 @@ import "./styles/ProjectSettings.scss";
 export default function ProjectSettings(props) {
   return (
     <div className="ProjectSettings">
+      <Box
+        className="infoZone"
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+      >
+        <RenameProjectPrompt project={props.project} />
+      </Box>
+
       <Heading as="h3">Danager Zone</Heading>
       <Box
         className="dangerZone"
