e70ecb7a8c951c96ebdbbf18242c3093acf51bff
Daniel <beta@ozx.me>
2022-12-11 17:12:14 +0000

    Added home page heading

--- app/pages/Home.js
+++ app/pages/Home.js
@@ -1,13 +1,16 @@
 import React from "react";
 
+import { Heading } from "@chakra-ui/react";
+
 import NewProjectPrompt from "../components/NewProjectPrompt";
 import ProjectsTable from "../components/ProjectsTable";
 
 export default function Home() {
   return (
     <div className="HomePage Page">
+      <Heading as="h2" size="lg">Home</Heading>
+
       <NewProjectPrompt />
-      
       <ProjectsTable />
     </div>
   );
