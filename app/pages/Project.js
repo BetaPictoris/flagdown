76f29055686269c3a208da87479d04e955910ac5
Daniel <beta@ozx.me>
2022-12-11 08:29:39 +0000

    Added loading spinner on project page This should fix some "undefined" calls

--- app/pages/Project.js
+++ app/pages/Project.js
@@ -7,41 +7,50 @@ import {
   TabPanels,
   Tab,
   TabPanel,
+  Spinner
 } from "@chakra-ui/react";
 
 import Flags from "../components/Flags";
 import ProjectSettings from "../components/ProjectSettings";
 
 export default function Project(props) {
+  const [projectLoaded, setProjectLoaded] = React.useState(false);
   const [project, setProject] = React.useState({});
 
   React.useEffect(() => {
     axios(`/api/v1/projects/${props.projectID}`).then((response) => {
       if (response.data) {
         setProject(response.data[0]);
+        setProjectLoaded(true);
       }
     });
   }, [props.projectID]);
 
   return (
     <div className="ProjectPage Page">
-      <Heading as="h2">{project.ProjectName}</Heading>
+      {projectLoaded ? ( 
+      <div>
+        <Heading as="h2">{project.ProjectName}</Heading>
 
-      <Tabs>
-        <TabList>
-          <Tab>Flags</Tab>
-          <Tab>Settings</Tab>
-        </TabList>
+        <Tabs>
+          <TabList>
+            <Tab>Flags</Tab>
+            <Tab>Settings</Tab>
+          </TabList>
 
-        <TabPanels>
-          <TabPanel>
-            <Flags project={project} />
-          </TabPanel>
-          <TabPanel>
-            <ProjectSettings project={project} />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
+          <TabPanels>
+            <TabPanel>
+              <Flags project={project} />
+            </TabPanel>
+            <TabPanel>
+              <ProjectSettings project={project} />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </div>
+      ) : (
+        <Spinner />
+      )}
     </div>
   );
 }
