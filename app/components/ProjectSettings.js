5dfe0edd653b0f80a39b83cc75396e3b5282a112
Daniel <beta@ozx.me>
2022-12-11 08:19:20 +0000

    Fixed projectPrompt button not loading

--- app/components/ProjectSettings.js
+++ app/components/ProjectSettings.js
@@ -7,11 +7,9 @@ import "./styles/ProjectSettings.scss"
 export default function ProjectSettings(props) {
   return (
     <div className="ProjectSettings">
-      <Heading as="h2">{props.project.ProjectName} settings</Heading>
-
       <Heading as="h3">Danager Zone</Heading>
       <Box className="dangerZone" borderWidth='1px' borderRadius='lg' overflow='hidden' borderColor='tomato'>
-        <DeleteProjectPrompt projectID={props.project.projectID} />
+        <DeleteProjectPrompt projectID={props.project.ProjectID} />
       </Box>
     </div>
   )
